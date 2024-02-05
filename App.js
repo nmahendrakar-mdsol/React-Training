5640641cb987c912e61da730cd36965b8d71b293
nmahendrakar-mdsol <nmahendrakar@mdsol.com>
2024-02-05 10:41:17 +0530

    adding bundler and updating gitignore file

--- App.js
+++ App.js
@@ -1,3 +1,15 @@
-const heading = React.createElement('h1', {id: "heading"}, 'welcome to homepage');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const heading = React.createElement('div', { id: 'parent' }, [
+	React.createElement('div', { id: 'child1' }, [
+		React.createElement('h1', {}, 'we re in child 1'),
+		React.createElement('h2', {}, 'we are in heading2 of child1'),
+	]),
+	React.createElement('div', { id: 'child2' }, [
+		React.createElement('h1', {}, 'we re in child 2'),
+		React.createElement('h2', {}, 'we are in heading2 of child2'),
+	]),
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
