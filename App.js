4b3c20ef3ab142c653e3df17441e7877ca5e576c
nmahendrakar-mdsol <nmahendrakar@mdsol.com>
2024-02-09 10:40:20 +0530

    React component and react elemtent creation

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+/*REACT ELEMENTS
+
 const heading = React.createElement('div', { id: 'parent' }, [
 	React.createElement('div', { id: 'child1' }, [
 		React.createElement('h1', {}, 'we re in child 1'),
@@ -13,3 +15,38 @@ const heading = React.createElement('div', { id: 'parent' }, [
 ]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
+*/
+// const heading = React.createElement('h1', { id: 'heading' }, 'Welcome to Medidata's react training');
+// console.log(heading);
+/*
+//JSX
+ const jsxHeading = <h1 id='heading>Welcome to Medidata's react training</h1>;
+console.log(jsxHeading);
+ //JSX is not html inside javascript, jsx provides a feature to create the reactelements similar to the html
+ //JSX is HTML like Syntax
+ //when we compare the heading and jsxheading both provide the same output as object
+
+*/
+
+//Creating a functional component
+
+const Title = () => {
+	return <h1>Welcome to Medidata's React Training</h1>;
+}
+
+//simplest way of writing it is so
+
+const HeadingComponent = () => (
+	<div id="container">
+		<Title />
+		<h1 className="heading-class">
+			We are learning functional components
+		</h1>
+	</div>
+);
+
+//we render a component wrapped in the brackets
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<HeadingComponent />
+);
